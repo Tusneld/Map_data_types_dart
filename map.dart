7f61e3a55b882920a56124b14e53a0552e7ec003
Tusnelde Endjala <tusneldelainae@gmail.com>
2024-03-15 00:24:53 +0200

    Map with String keys and int values

---
+++ map.dart
@@ -0,0 +1,7 @@
+void main(){
+  //Creating a map with string keys and int values
+  Map<String, int>ages = {
+    'Tusnelde': 30, 'Bob': 25, 'Alice': 27
+  };
+  print("ages of the students: $ages");
+}
